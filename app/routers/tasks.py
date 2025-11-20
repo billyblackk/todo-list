6f94cf540f16e5d7a1c8498326ef9323d0124424
Milan <work.milandave@gmail.com>
2025-11-20 16:19:52 +0000

    Stop tracking todo.db and ignore local database

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.core.database import get_db
 from app.models.task import Task as TaskModel
-from app.schemas.task import Task, TaskCreate
+from app.schemas.task import Task, TaskCreate, TaskUpdate
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
@@ -23,3 +23,50 @@ def create_task(task_in: TaskCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(task)
     return task
+
+
+@router.get("/{task_id}", response_model=Task)
+def get_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(TaskModel).filter(TaskModel.id == task_id).first()
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Task with id {task_id} not found.",
+        )
+    return task
+
+
+@router.patch("/{task_id}", response_model=Task)
+def update_task(task_id: int, task_in: TaskUpdate, db: Session = Depends(get_db)):
+    task = db.query(TaskModel).filter(TaskModel.id == task_id).first()
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Task with id {task_id} not found",
+        )
+
+    # We will only update the fields that were sent
+    update_data = task_in.model_dump(exclude_unset=True)
+
+    for field, value in update_data.items():
+        setattr(task, field, value)
+
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+
+    return task
+
+
+@router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = db.query(TaskModel).filter(TaskModel.id == task_id).first()
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Task with id {task_id} not found.",
+        )
+
+    db.delete(task)
+    db.commit()
+    return
